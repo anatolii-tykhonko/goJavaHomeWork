2ee68db84f389ffef80ae068f3c1e2a077829520
Dranatom <zorg.anatom1990@gmail.com>
2017-01-30 19:37:38 +0200

    Initial commit

---
+++ src/main/java/com/anatoliy/practice/modul01/task011/FirstClass.java
@@ -0,0 +1,8 @@
+package com.anatoliy.practice.modul01.task011;
+
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("My fist programs on Java...");
+    }
+}
---
+++ src/main/java/com/anatoliy/practice/modul01/task011/SecondClass.java
@@ -0,0 +1,8 @@
+package com.anatoliy.practice.modul01.task011;
+
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("My fist programs on Java...");
+    }
+}
