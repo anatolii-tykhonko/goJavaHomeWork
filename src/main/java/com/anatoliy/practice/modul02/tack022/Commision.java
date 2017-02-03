70ff257621e91597b2538c78f301c6db7fa44c95
Dranatom <zorg.anatom1990@gmail.com>
2017-02-03 17:40:16 +0200

    ДЗ2.2

---
+++ src/main/java/com/anatoliy/practice/modul02/tack022/Commision.java
@@ -0,0 +1,19 @@
+package com.anatoliy.practice.modul02.tack022;
+
+
+public class Commision {
+    public static void main(String[] args) {
+        int balance = 100;
+        int withdrawal = 10;
+        double commisionByPercent = 5.0;
+        Commision commision = new Commision();
+        commision.withdraw(balance, withdrawal, commisionByPercent);
+    }
+
+    public void withdraw(int balance, int withdrawal, double commisionByPercent) {
+        double commisionByMoney = withdrawal * commisionByPercent / 100;
+        if ((commisionByMoney + withdrawal) < balance)
+            System.out.println("OK " + commisionByMoney + " " + (balance - commisionByMoney));
+        else System.out.println("NO");
+    }
+}
