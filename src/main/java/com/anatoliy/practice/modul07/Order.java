4ff6cfa9da8105d4e4e68ac21cde98e4f54841ba
Dranatom <zorg.anatom1990@gmail.com>
2017-03-28 20:44:09 +0300

    дз7

---
+++ src/main/java/com/anatoliy/practice/modul07/Order.java
@@ -0,0 +1,18 @@
+package com.anatoliy.practice.modul07;
+
+/**
+ * Created by anaatolii on 28.03.17.
+ */
+public class Order {
+
+
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+
+    private enum Currency {
+    }
+}
---
+++ src/main/java/com/anatoliy/practice/modul07/User.java
@@ -0,0 +1,12 @@
+package com.anatoliy.practice.modul07;
+
+/**
+ * Created by anaatolii on 28.03.17.
+ */
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int balance;
+}
