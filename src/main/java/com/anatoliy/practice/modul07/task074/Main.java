84c2b40465ba7f14146f9e8d2bb46a1c4832aa98
Dranatom <zorg.anatom1990@gmail.com>
2017-03-31 20:45:27 +0300

    дз7

--- src/main/java/com/anatoliy/practice/modul07/task074/Main.java
+++ src/main/java/com/anatoliy/practice/modul07/task074/Main.java
@@ -3,30 +3,90 @@ package com.anatoliy.practice.modul07.task074;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
+import java.util.List;
 
-/**
- * Created by anaatolii on 30.03.17.
- */
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> arrayListString = new ArrayList<String>();
+        arrayListString.add("sssss");
         ArrayList<Integer> arrayListInteger = new ArrayList<Integer>();
+        arrayListInteger.add(22);
         LinkedList<String> linkedListString = new LinkedList<String>();
+        linkedListString.add("sda");
         LinkedList<Integer> linkedListInteger = new LinkedList<Integer>();
+        linkedListInteger.add(12);
+        printTimeToAddInList(arrayListInteger);
+        printTimeToAddInList(linkedListInteger);
+        printTimeToAddInList(arrayListString);
+        printTimeToAddInList(linkedListString);
 
-        Date beginAddIntegerArrayList = new Date();
-        for (int i = 0; i < 10000; i++) {
-            arrayListInteger.add(i);
-        }
-        Date endAddIntegerArrayList = new Date();
-        System.out.println("Время для добавления arrayList = " + (endAddIntegerArrayList.getTime() - beginAddIntegerArrayList.getTime()));
+        printTimeToSetInList(arrayListInteger);
+        printTimeToSetInList(linkedListInteger);
+        printTimeToSetInList(arrayListString);
+        printTimeToSetInList(linkedListString);
 
-        Date beginGetIntegerArrayList = new Date();
-        for (int i = 0; i < 10000; i++) {
-            int j = arrayListInteger.size() / 2;
-            arrayListInteger.set(j, i);
-        }
-        Date endGetIntegerArrayList = new Date();
-        System.out.println("Время для добавления в середину arrayList = " + (endGetIntegerArrayList.getTime() - beginGetIntegerArrayList.getTime()));
+        printTimeToGetInList(arrayListInteger);
+        printTimeToGetInList(linkedListInteger);
+        printTimeToGetInList(arrayListString);
+        printTimeToGetInList(linkedListString);
+
+        printTimeToRemoveInList(arrayListInteger);
+        printTimeToRemoveInList(linkedListInteger);
+        printTimeToRemoveInList(arrayListString);
+        printTimeToRemoveInList(linkedListString);
+
+
+
+
     }
+
+    public static void printTimeToAddInList(List list) {
+        Object o;
+        if (list.get(0) instanceof Integer) {
+            o = 200;
+        } else {
+            o = "sssss";
+        }
+        Date beginAddList = new Date();
+        for (int i = 0; i < 10000; i++) {
+            list.add(o);
+        }
+        Date endAddList = new Date();
+        System.out.println("Время для добавления " + list.getClass().getSimpleName() + " = " + (endAddList.getTime() - beginAddList.getTime()));
+    }
+
+    public static void printTimeToSetInList(List list) {
+        Object o;
+        if (list.get(0) instanceof Integer) {
+            o = 200;
+        } else {
+            o = "sssss";
+        }
+        Date beginSetList = new Date();
+        for (int i = 0; i < 10000; i++) {
+            int j = list.size() / 2;
+            list.set(j, o);
+        }
+        Date endSetList = new Date();
+        System.out.println("Время для добавления в середину " + list.getClass().getSimpleName() + " = " + (endSetList.getTime() - beginSetList.getTime()));
+    }
+
+    public static void printTimeToGetInList(List list) {
+        Date beginGetList = new Date();
+        for (int i = 0; i < 10000; i++) {
+            list.get(list.size() / 2 - 1);
+        }
+        Date endGetList = new Date();
+        System.out.println("Время для вывода элемента " + list.getClass().getSimpleName() + " = " + (endGetList.getTime() - beginGetList.getTime()));
+    }
+
+    public static void printTimeToRemoveInList(List list) {
+        Date beginRemoveList = new Date();
+        for (int i = 0; i < 10000; i++) {
+            list.remove(0);
+        }
+        Date endRemoveList = new Date();
+        System.out.println("Время для удаления элемента " + list.getClass().getSimpleName() + " = " + (endRemoveList.getTime() - beginRemoveList.getTime()));
+    }
+
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,13 +5,7 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        String[] s = new String[5];
-        s[1] = "sds";
-        s[3] = "sdsss";
-        Arrays.sort(s);
-
-        for (int i = 0; i < s.length; i++) {
-            System.out.println(s[i]);
-        }
+        Integer a = 5;
+        System.out.println(a instanceof Integer);
     }
 }
